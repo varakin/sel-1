bd44c1acbda0f4543790bad563f3c3d02f3d77fb
varakin <varakin_ivan@yahoo.com>
2016-11-28 19:47:56 +0300

    задание 1
    задание 1
    попытка 1

---
+++ first.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.starline-online.ru")
