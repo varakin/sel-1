7b4b343174b3166c48100f4956072e1b433cf8fd
varakin <varakin_ivan@yahoo.com>
2016-12-12 20:19:31 +0300

    Задание 9 (1) попытка 2

--- exercise_9_1.py
+++ exercise_9_1.py
@@ -9,12 +9,6 @@ def driver(request):
     return wd
 
 
-def check_alph(collection):
-    texts = [text for text in collection]
-
-    return texts == sorted(texts)
-
-
 def test_example(driver):
     driver.implicitly_wait(10)
     driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
@@ -32,12 +26,12 @@ def test_example(driver):
         if int(zones_numbers[i]) != 0:
             driver.get(urls[i])
             cities = driver.find_elements_by_css_selector("#table-zones tr:not(:last-child) td:nth-child(3)")
+            city_names = [city.text for city in cities]
 
-            if not check_alph([city.text for city in cities]):
-                return False
+            assert city_names == sorted(city_names)
 
             driver.back()
 
-    return check_alph(names)
+    return names == sorted(names)
 
 
